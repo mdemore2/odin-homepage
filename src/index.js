e8960ef94500a300c36c6096cb790f46b91cdcc7
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2024-10-20 15:26:24 -0400

    setting up basic page layout, need to fix webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("test");
+import "./style.css";
+
+buildPage();
+
+function buildPage() {
+  var body = document.querySelector("body");
+  var about = document.createElement("div");
+  about.classList.add("about");
+  var work = document.createElement("div");
+  work.classList.add("work");
+  var contact = document.createElement("div");
+  contact.classList.add("contact");
+
+  body.appendChild(about);
+  body.appendChild(work);
+  body.appendChild(contact);
+}
